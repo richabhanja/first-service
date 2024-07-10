5e79d15211786e924fef1da7866713d9b77accbc
unknown <richajain.it18@gmail.com>
2024-07-10 19:13:38 +0530

    controller, docker file, data source ignore

---
+++ src/main/java/com/first/microservice/first_service/TestController.java
@@ -0,0 +1,18 @@
+package com.first.microservice.first_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/") public String health()
+    {
+        return "Hello Geeks And Welcome to GeekForGeeks !!!";
+    }
+
+    @GetMapping("/test") public String test()
+    {
+        return "Hello Geeks And Welcome to GeekForGeeks !!!";
+    }
+}
